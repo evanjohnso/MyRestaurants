02f7572f6304bf4c1a025cf5a685db87d8c1b955
ej <evanrjohnso@gmail.com>
2017-10-07 16:25:17 -0700

    pass data from one activity to the next

--- app/src/main/java/com/evanrjohnso/myrestaurants/MainActivity.java
+++ app/src/main/java/com/evanrjohnso/myrestaurants/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
 
@@ -26,9 +25,9 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();
-                Log.d(TAG, location);
+                String userLocation = mLocationEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", userLocation);
                 startActivity(intent);
             }
         });
