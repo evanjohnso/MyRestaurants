26f3b1babcf82d479b80c7617e767c8baefee81f
ej <evanrjohnso@gmail.com>
2017-10-08 18:07:17 -0700

    add custom font to main activity

--- app/src/main/java/com/evanrjohnso/myrestaurants/MainActivity.java
+++ app/src/main/java/com/evanrjohnso/myrestaurants/MainActivity.java
@@ -1,17 +1,22 @@
 package com.evanrjohnso.myrestaurants;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +25,11 @@ public class MainActivity extends AppCompatActivity {
 
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
 
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener()  {
-
             @Override
             public void onClick(View v) {
                 String userLocation = mLocationEditText.getText().toString();
