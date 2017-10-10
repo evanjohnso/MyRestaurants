5db3cfe8c38c7e423b0444b8193163f66d063ddd
ej <evanrjohnso@gmail.com>
2017-10-10 08:04:17 -0700

    add butterknife binding

--- app/src/main/java/com/evanrjohnso/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/evanrjohnso/myrestaurants/RestaurantsActivity.java
@@ -12,9 +12,12 @@ import android.widget.Toast;
 
 import java.lang.reflect.Array;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class RestaurantsActivity extends AppCompatActivity {
-    private TextView mLocationTextView;
-    private ListView mListView;
+    @Bind(R.id.locationTextView) TextView mLocationTextView;
+    @Bind(R.id.listView) ListView mListView;
     private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
             "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
             "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
@@ -25,9 +28,8 @@ public class RestaurantsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
-        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
-        mListView = (ListView) findViewById(R.id.listView);
 
+        ButterKnife.bind(this);
         ArrayAdapter myAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
         mListView.setAdapter(myAdapter);
 
